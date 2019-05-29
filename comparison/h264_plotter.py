bce5554f7e07801bfe2fa68406aa60eb03929d54
Marko Stanoevich <MStanoevich@gmail.com>
2019-05-29 16:53:27 +0200

    Add all presets to plotting script

--- comparison/h264_plotter.py
+++ comparison/h264_plotter.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 import csv
+import numpy as np
 
 #h264
 # with open("h264_results/k_metrics_for_plotting.csv") as f:
@@ -9,57 +10,75 @@ import csv
 #     headers = next(f_csv)
 #     for row in f_csv:
 #         k_ufSSIM = float(row[0])
-#         k_ufPNSR = float(row[1])
+#         k_ufPSNR = float(row[1])
 #         k_sfSSIM = float(row[2])
-#         k_sfPNSR = float(row[3])
+#         k_sfPSNR = float(row[3])
 #         k_vfSSIM = float(row[4])
-#         k_vfPNSR = float(row[5])
+#         k_vfPSNR = float(row[5])
 #         k_ferSSIM = float(row[6])
-#         k_ferPNSR = float(row[7])
+#         k_ferPSNR = float(row[7])
 #         k_fSSIM = float(row[8])
-#         k_fPNSR = float(row[9])
+#         k_fPSNR = float(row[9])
 #         k_mSSIM = float(row[10])
-#         k_mPNSR = float(row[11])
+#         k_mPSNR = float(row[11])
+#         k_sSSIM = float(row[12])
+#         k_sPSNR = float(row[13])
+#         k_serSSIM = float(row[14])
+#         k_serPSNR = float(row[15])
+#         k_vsSSIM = float(row[16])
+#         k_vsPSNR = float(row[17])
 
 with open("h264_results/confidence_metrics_for_plotting.csv") as f:
     f_csv = csv.reader(f)
     headers = next(f_csv)
     for row in f_csv:
         ufSSIM_conf = float(row[0])
-        ufPNSR_conf = float(row[1])
+        ufPSNR_conf = float(row[1])
         sfSSIM_conf = float(row[2])
-        sfPNSR_conf = float(row[3])
+        sfPSNR_conf = float(row[3])
         vfSSIM_conf = float(row[4])
-        vfPNSR_conf = float(row[5])
+        vfPSNR_conf = float(row[5])
         ferSSIM_conf = float(row[6])
-        ferPNSR_conf = float(row[7])
+        ferPSNR_conf = float(row[7])
         fSSIM_conf = float(row[8])
-        fPNSR_conf = float(row[9])
+        fPSNR_conf = float(row[9])
         mSSIM_conf = float(row[10])
-        mPNSR_conf = float(row[11])
+        mPSNR_conf = float(row[11])
+        sSSIM_conf = float(row[12])
+        sPSNR_conf = float(row[13])
+        serSSIM_conf = float(row[14])
+        serPSNR_conf = float(row[15])
+        vsSSIM_conf = float(row[16])
+        vsPSNR_conf = float(row[17])       
 
 # with open("h264_results/size_metrics_for_plotting.csv") as f:
 #     f_csv = csv.reader(f)
 #     headers = next(f_csv)
 #     for row in f_csv:
 #         ufSSIM_size = float(row[0])
-#         ufPNSR_size = float(row[1])
+#         ufPSNR_size = float(row[1])
 #         sfSSIM_size = float(row[2])
-#         sfPNSR_size = float(row[3])
+#         sfPSNR_size = float(row[3])
 #         vfSSIM_size = float(row[4])
-#         vfPNSR_size = float(row[5])
+#         vfPSNR_size = float(row[5])
 #         ferSSIM_size = float(row[6])
-#         ferPNSR_size = float(row[7])
+#         ferPSNR_size = float(row[7])
 #         fSSIM_size = float(row[8])
-#         fPNSR_size = float(row[9])
+#         fPSNR_size = float(row[9])
 #         mSSIM_size = float(row[10])
-#         mPNSR_size = float(row[11])
+#         mPSNR_size = float(row[11])
+#         sSSIM_size = float(row[12])
+#         sPSNR_size = float(row[13])
+#         serSSIM_size = float(row[14])
+#         serPSNR_size = float(row[15])
+#         vsSSIM_size = float(row[16])
+#         vsPSNR_size = float(row[17])
 
-#k = [k_ufSSIM, k_ufPNSR, k_sfSSIM, k_sfPNSR, k_vfSSIM, k_vfPNSR, k_ferSSIM, k_ferPNSR, k_fSSIM, k_fPNSR, k_mSSIM, k_mPNSR]
-confidence = [ufSSIM_conf, ufPNSR_conf, sfSSIM_conf, sfPNSR_conf, vfSSIM_conf, vfPNSR_conf, ferSSIM_conf, ferPNSR_conf, fSSIM_conf, fPNSR_conf, mSSIM_conf, mPNSR_conf]
-#size = [ufSSIM_size, ufPNSR_size, sfSSIM_size, sfPNSR_size, vfSSIM_size, vfPNSR_size, ferSSIM_size, ferPNSR_size, fSSIM_size, fPNSR_size, mSSIM_size, mPNSR_size]
-codec = ["ultrafast SSIM", "ultrafast PNSR", "superfast SSIM", "superfast PNSR", "veryfast SSIM", "veryfast PNSR", "faster SSIM", "faster PNSR", "fast SSIM", "fast PNSR", "medium SSIM", "medium PNSR"]
-baseline = [1,1,1,1,1,1,1,1,1,1,1,1]
+#k = [k_ufSSIM, k_ufPSNR, k_sfSSIM, k_sfPSNR, k_vfSSIM, k_vfPSNR, k_ferSSIM, k_ferPSNR, k_fSSIM, k_fPSNR, k_mSSIM, k_mPSNR, k_sSSIM, k_sPSNR, k_serSSIM, k_serPSNR, k_vsSSIM, k_vsPSNR]
+confidence = [ufSSIM_conf, ufPSNR_conf, sfSSIM_conf, sfPSNR_conf, vfSSIM_conf, vfPSNR_conf, ferSSIM_conf, ferPSNR_conf, fSSIM_conf, fPSNR_conf, mSSIM_conf, mPSNR_conf, sSSIM_conf, sPSNR_conf, serSSIM_conf, serPSNR_conf, vsSSIM_conf, vsSSIM_conf]
+#size = [ufSSIM_size, ufPSNR_size, sfSSIM_size, sfPSNR_size, vfSSIM_size, vfPSNR_size, ferSSIM_size, ferPSNR_size, fSSIM_size, fPSNR_size, mSSIM_size, mPSNR_size, sSSIM_size, sPSNR_size, serSSIM_size, serPSNR_size, vsSSIM_size, vsPSNR_size]
+codec = ["ultrafast SSIM", "ultrafast PSNR", "superfast SSIM", "superfast PSNR", "veryfast SSIM", "veryfast PSNR", "faster SSIM", "faster PSNR", "fast SSIM", "fast PSNR", "medium SSIM", "medium PSNR", "slow SSIM", "slow PSNR", "slower SSIM", "slower PSNR", "veryslow SSIM", "veryslow PSNR"]
+#baseline = [1,1,1,1,1,1,1,1,1,1,1,1]
 
 # To plot k metric
 #plt.plot(codec, k)
@@ -68,9 +87,9 @@ baseline = [1,1,1,1,1,1,1,1,1,1,1,1]
 #plt.ylabel('K metric')
 
 # To plot confidence performance
-plt.plot(codec, confidence)
-plt.xlabel('h264 preset & tune')
-plt.ylabel('Performance % compared to original')
+# plt.plot(codec, confidence)
+# plt.xlabel('h264 preset & tune')
+# plt.ylabel('Performance % compared to original')
 
 # To plot size 
 # plt.plot(codec, size)
@@ -79,6 +98,11 @@ plt.ylabel('Performance % compared to original')
 
 # TODO finnish
 # To plot barchart performance
-#plt.bart(performance, codec, align = 'center', alpha=0.5)
+index = np.arange(len(codec))
+plt.plot(index, confidence)
+plt.xlabel('h264 preset & tune')
+plt.ylabel('Performance % compared to original')
+plt.xticks(index, codec, fontsize=10, rotation=30)
+plt.title('Neural network performance on compressed anotated dataset')
 
 plt.show()
